a50461c32393f79ca5b5fce21951dac89cf13a20
JasimeS98 <jasun@tyro.com>
2024-06-26 08:41:55 +1000

    remove script

--- .ci_steps/version-bump.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-bump_type=$1
-REPO="https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.git"
-
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-npm version "$bump_type" -m "update package to version %s [skip cli]" && git push "https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.git"
